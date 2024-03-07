a2fe2d4ca918ae34c772d69a77a8387e664ceda5
AngelAlejandroVilla <skrach12a@gmail.com>
2024-03-06 19:22:32 -0600

    Estilos Form 1/2

--- src/views/Forms/FormAnswer.vue
+++ src/views/Forms/FormAnswer.vue
@@ -1,119 +1,75 @@
 <template>
-  <div class="container">
-    <div class="form-response">
-      <h2>titulo</h2>
-    </div>
-    <div class="form-response">
-      <p>descripcion</p>
-    </div>
-
-    <form @submit.prevent="submitResponse">
-      <div class="form-group">
-        <label :for="'question-' + index" class="form-label">pregunta</label>
-        <input
-          type="text"
-          class="form-input"
-          placeholder="Ingrese su respuesta"
-        />
+  <div>
+    <div class="container mt-10 px-12">
+      <div class="text-center">
+        <span class="card text-uppercase py-6 fw-bold" style="font-size: 3rem"
+          >formulario</span
+        >
       </div>
-      <button type="submit" class="btn btn-submit">Enviar Respuestas</button>
-    </form>
-  </div>
-</template>
+      <div class="card mt-5">
+        <div class="row">
+          <div class="col-md-5">
+            <img
+              src="https://img.freepik.com/vector-premium/concepto-prueba-caracter-personas-que-responden-lista-verificacion-cuestionarios-resumen-resultados-exito-examen-linea-formulario-cuestionario-educacion-linea-metafora-encuesta_269730-429.jpg"
+              class="img-fluid img-thumbnai text-center"
+              style="width: 24rem; height: 24rem; object-fit: cover"
+            />
+          </div>
+          <div class="col-md-5">
+            <div
+              class="d-flex justify-content-center align-content-center flex-wrap px-12 fs-4"
+              style="color: #696969"
+            >
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Reiciendis eveniet dignissimos nobis optio qui ullam nemo a
+                voluptates enim consectetur rem quas, ipsam iste dolore velit
+                asperiores architecto eaque minima.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-<!-- <template>
-    <div class="form-response">
-      <h2>{{ title }}</h2>
-      <p>{{ description }}</p>
+    <div class="container">
       <form @submit.prevent="submitResponse">
-        <div v-for="(question, index) in questions" :key="index" class="form-group">
-          <label :for="'question-' + index" class="form-label">{{ question }}</label>
+        <div class="card mt-12 p-8">
+          <label
+            :for="'question-' + index"
+            class="fs-3 mb-2"
+            style="font-weight: 500"
+            >pregunta</label
+          >
           <input
             type="text"
-            :id="'question-' + index"
-            v-model="responses[index]"
             class="form-input"
             placeholder="Ingrese su respuesta"
           />
         </div>
-        <button type="submit" class="btn btn-submit">Enviar Respuestas</button>
+        <div class="mt-12 d-flex justify-content-center">
+          <button type="submit" class="btn btn-primary">
+            Enviar Respuestas
+          </button>
+        </div>
       </form>
     </div>
-  </template> -->
-
-<script>
-// export default {
-//   props: {
-//     title: String,
-//     description: String,
-//     pregunta: Array,
-//   },
-//   data() {
-//     return {
-//       responses: Array(this.questions.length).fill(""),
-//     };
-//   },
-//   methods: {
-//     submitResponse() {
-//       // Aquí puedes manejar el envío de las respuestas
-//       console.log("Respuestas enviadas:", this.responses);
-//       // Puedes realizar alguna acción adicional aquí, como enviar las respuestas a través de una solicitud HTTP
-//     },
-//   },
-// };
-</script>
+  </div>
+</template>
 
 <style scoped>
-.container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-}
-.form-response {
-  max-width: 500px;
-  margin: 0 auto;
-  margin-top: 10px;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-}
-
-.form-response h2 {
-  margin-bottom: 10px;
-}
-
-.form-response p {
-  margin-bottom: 20px;
-}
-
-.form-group {
-  margin-bottom: 20px;
-}
-
-.form-label {
-  font-weight: bold;
-  margin-bottom: 5px;
-}
-
 .form-input {
-  width: 100%;
+  width: calc(100% - 40px); /* Restar el ancho del botón de eliminar */
   padding: 8px;
   font-size: 16px;
   border: 1px solid #ccc;
   border-radius: 4px;
+  outline: none; /* Remueve el borde de foco predeterminado */
+  transition: border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out; /* Suaviza la transición de bordes y sombras */
 }
 
-.btn {
-  padding: 10px 20px;
-  font-size: 16px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  background-color: #008cba;
-  color: white;
-}
-
-.btn-submit:hover {
-  opacity: 0.8;
+.form-input:focus {
+  border-color: #66afe9; /* Cambia el color del borde al enfocar */
+  box-shadow: 0 0 8px rgba(102, 175, 233, 0.6); /* Agrega un sombreado sutil */
 }
 </style>
--- src/views/Transacciones.vue
+++ src/views/Transacciones.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>erferg</div>
+  <div>
+    <TableTransacciones />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TableTransacciones from "@/components/widgets/tables/Widget11.vue";
+
+export default defineComponent({
+  name: "Transacciones",
+  components: {
+    TableTransacciones,
+  },
+  setup() {},
+});
+</script>
